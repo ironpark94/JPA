19bc82b219caa6ea33c1014afc0dd0f04939a45d
pci <ironman94@naver.com>
2024-08-18 21:32:25 +0900

    upload

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.repository;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+
+@Repository
+public class MemberRepository {
+    
+    @PersistenceContext
+    EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
